afc0b21dab450629297694bbcd6c1d97918b0474
ANKITA-web34 <anki333soul@gmail.com>
2021-02-05 19:11:51 +0530

    gallery section

--- script.js
+++ script.js
@@ -24,4 +24,18 @@ $(document).ready(function() {
         }
     });
 
+    $('.gallery-list-item').click(function() {
+        let value = $(this).attr('data-filter');
+        if(value === 'All') {
+            $('.filter').show(300);
+        } else {
+            $('.filter').not('.' + value).hide(300);
+            $('.filter').filter('.' + value).show(300);
+        }
+    });
+
+    $('.gallery-list-item').click(function() {
+        $(this).addClass('active-item').siblings().removeClass('active-item');
+    });
+
 });
